99ba9c48a5fb626a91b306746865159907eabbd7
farabikhan0412 <ahmedshakil0512@gmail.com>
2022-01-07 01:25:23 +0600

    files added

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ document.querySelector('#signup-btn').onclick = () =>{
 
 window.onscroll = () =>{
    
-
   // searchForm.classList.remove('active');
 
   if(window.scrollY > 80){
@@ -34,4 +33,35 @@ window.onload = () =>{
 
   }
 }
+let loginForm = document.querySelector('.login-form-container');
+document.querySelector('#signup-btn').onclick = () =>{
 
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+
+  loginForm.classList.remove('active');
+}
+
+
+var swiper = new Swiper(".books-slider", {
+  
+  loop:true,
+  centeredSlider:true,
+  autoplay: {
+    delay:9500,
+    disableOnInteraction:false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
